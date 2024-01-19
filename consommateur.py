5119d8835dfcf953cb633ee40b750568159a230e
HashiWork <115980850+HashiWork@users.noreply.github.com>
2024-01-19 11:33:06 +0100

    Update consommateur.py

--- consommateur.py
+++ consommateur.py
@@ -1,24 +1,24 @@
 from kafka import KafkaConsumer
 import json
 
-# Configuration du consommateur Kafka
+# Configuration du consommateur
 consumer = KafkaConsumer(
     'meteo',
     bootstrap_servers='localhost:9092',
     auto_offset_reset='earliest',  # Commence à lire depuis le début si aucun offset n'est enregistré
-    enable_auto_commit=False,      # Désactive le commit automatique des offsets
     group_id='groupe_consommateur_meteo',  # Identifie le groupe de consommateurs
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))  # Désérialise les données JSON reçues
 )
 
+def process_message(message):
+    # Vous pouvez ajouter ici une logique de traitement supplémentaire si nécessaire
+    print(f"Clé: {message.key}, Données Météo: {message.value}")
+
 def main():
     try:
         for message in consumer:
-            # Affichage des données et de la clé
-            print(f"Clé: {message.key}, Données: {message.value}")
-
-            # Commit explicite de l'offset
-            consumer.commit()
+            process_message(message)
+            consumer.commit()  # Commit explicitement l'offset après l'affichage
     except KeyboardInterrupt:
         print("Arrêt du consommateur")
     finally:
