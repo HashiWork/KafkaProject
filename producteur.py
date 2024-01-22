b3ec39fb479beb7c533cdfeccd61162f76c1192d
HashiWork <115980850+HashiWork@users.noreply.github.com>
2024-01-22 14:49:39 +0100

    Update producteur.py

--- producteur.py
+++ producteur.py
@@ -1,17 +1,19 @@
+import os
 import json
 from kafka import KafkaProducer
-import time
 import requests
 from datetime import datetime
-from pathlib import Path
+from bs4 import BeautifulSoup
 
 # Configuration du producteur Kafka
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          key_serializer=str.encode,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+METEO_URL = "http://192.168.1.43/meteo/"
+
 def fetch_data_from_server(filename):
-    url = f"http://192.168.1.43/meteo/{filename}"
+    url = f"{METEO_URL}{filename}"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -19,35 +21,41 @@ def fetch_data_from_server(filename):
     else:
         return None
 
-def filename_to_timestamp(filename):
-    # Extraire l'heure du nom du fichier et la transformer
-    file_name = Path(filename).stem
-    file_hour = file_name[13:26]
-    return file_hour
+def get_filenames_from_webpage(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = soup.find_all('a')
+        filenames = [link.get('href') for link in links if link.get('href').endswith('.json')]
+        return filenames
+    else:
+        return []
+
+def filename_to_datetime(filename):
+    parts = filename.split('_')
+    date_part = parts[2]  # '22-01-2024'
+    hour_part = parts[3].split('.')[0]  # '14'
+    datetime_str = f"{date_part}_{hour_part}"
+    return datetime.strptime(datetime_str, '%d-%m-%Y_%H')
 
 def calculate_averages(data):
-    # Assurez-vous que ces clés existent dans vos données
     if 'current' in data:
         current_data = data['current']
-        temp = current_data.get('temperature', 0)
-        humidity = current_data.get('humidity', 0)
-        return {'avg_temp': temp, 'avg_humidity': humidity}
+        return {'avg_temp': current_data.get('temperature', 0), 'avg_humidity': current_data.get('humidity', 0)}
     return None
 
 def run_producer():
-    while True:
-        filename = f"weather_data_22-01-2024_14.json"
+    filenames = get_filenames_from_webpage(METEO_URL)
+    sorted_filenames = sorted(filenames, key=filename_to_datetime)
+    for filename in sorted_filenames:
         data = fetch_data_from_server(filename)
-
         if data:
             averages = calculate_averages(data)
             if averages:
                 datas = str(averages) + " " + str(data)
-                timestamp = filename_to_timestamp(filename)
+                timestamp = filename_to_datetime(filename).strftime('%Y-%m-%d %H:%M:%S')
                 producer.send('meteo', key=timestamp, value=datas)
                 print(f"Message envoyé avec timestamp {timestamp} et moyennes {averages} et données : {data}")
-
-        time.sleep(1)  # Attendre une minute avant de chercher les nouvelles données
 
 if __name__ == "__main__":
     run_producer()
